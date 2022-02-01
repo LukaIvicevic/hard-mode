87528ceeb773b68dc5719d62134c2e23c77d2b0a
Ivicevic, Luka (S&T-Student) <li4g5@mst.edu>
2022-01-31 19:42:29 -0600

    Difficulty on main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,9 +10,15 @@ public class MainMenu : MonoBehaviour
     [SerializeField]
     private Slider volumeSlider;
 
+    [SerializeField]
+    private TextMeshProUGUI highestDifficultyText;
+
+    private string highestDifficultyKey = "HighestDifficulty";
+
     private void Start()
     {
         volumeSlider.value = SettingsManager.Instance.Volume;
+        highestDifficultyText.text = PlayerPrefs.GetFloat(highestDifficultyKey, 0).ToString();
     }
 
     public void Play()
